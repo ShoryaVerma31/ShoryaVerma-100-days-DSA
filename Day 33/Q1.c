8ff118d56b7df70406dbda79899f9e56fd1e6f75
Shorya Verma <shoryaverma3105@gmail.com>
2026-03-05 19:45:52 +0530

    Daily practice question

---
+++ Day 33/Q1.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char x) {
+    stack[++top] = x;
+}
+
+char pop() {
+    if (top == -1)
+        return -1;
+    return stack[top--];
+}
+
+// Check precedence
+int precedence(char x) {
+    if (x == '^')
+        return 3;
+    else if (x == '*' || x == '/')
+        return 2;
+    else if (x == '+' || x == '-')
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i, j = 0;
+
+    printf("Enter Infix Expression: ");
+    scanf("%s", infix);
+
+    for (i = 0; infix[i] != '\0'; i++) {
+
+        // If operand, add to postfix
+        if (isalnum(infix[i])) {
+            postfix[j++] = infix[i];
+        }
+
+        // If '(' push
+        else if (infix[i] == '(') {
+            push(infix[i]);
+        }
+
+        // If ')', pop until '('
+        else if (infix[i] == ')') {
+            while (stack[top] != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); // remove '('
+        }
+
+        else {
+            while (top != -1 && 
+                   precedence(stack[top]) >= precedence(infix[i])) {
+                postfix[j++] = pop();
+            }
+            push(infix[i]);
+        }
+    }
+
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("Postfix Expression: %s\n", postfix);
+
+    return 0;
+}
